7149919342f6a447835f5f335623da4b10c38242
chriskoh12 <chriskoh12@gmail.com>
2020-04-15 16:56:52 -0400

    created Team class

---
+++ src/app/models/Team.ts
@@ -0,0 +1,13 @@
+export class Team {
+    teamName: string;
+    teamLeader: string;
+    roster: Player[];
+    bench: Player[]
+};
+
+export class Player {
+    name: string;
+    position: Position;
+}
+
+export type Position = 'top' | 'jng' | 'mid' | 'adc' | 'sup';
