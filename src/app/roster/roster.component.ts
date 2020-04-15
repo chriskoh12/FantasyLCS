de8a1e42bc2839a10f8f0058bb1f9c274c3338d5
chriskoh12 <chriskoh12@gmail.com>
2020-04-15 16:57:33 -0400

    created sample data and started layout

--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Team, Player, Position } from '../models/Team';
 
 @Component({
   selector: 'app-roster',
@@ -8,35 +9,109 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 })
 export class RosterComponent implements OnInit {
 
-  roster = [
-    'Bwipo',
-    'Selfmade',
-    'Nemesis',
-    'Rekkles',
-    'Hyllissang'
+  positions: string[] = [
+    'top',
+    'jng',
+    'mid',
+    'adc',
+    'sup'
+  ]
+
+  rosters: Player[][] = [
+    [
+      { name: 'Impact', position: 'top' }, 
+      { name: 'Broxah', position: 'jng' },
+      { name: '', position: 'mid' }, 
+      { name: 'Doublelift', position: 'adc' }, 
+      { name: 'CoreJJ', position: 'sup'}
+    ],
+    [
+      { name: 'Impact', position: 'top' }, 
+      { name: 'Broxah', position: 'jng' },
+      { name: 'Jensen', position: 'mid' }, 
+      { name: 'Doublelift', position: 'adc' }, 
+      { name: 'CoreJJ', position: 'sup'}
+    ],
+    [
+      { name: 'Impact', position: 'top' }, 
+      { name: 'Broxah', position: 'jng' },
+      { name: 'Jensen', position: 'mid' }, 
+      { name: 'Doublelift', position: 'adc' }, 
+      { name: 'CoreJJ', position: 'sup'}
+    ]
+    // ['BrokenBlade', 'Dardoch', 'Bjergsen', 'Kobbe', 'Biofrost'],
+    // ['Licorice', 'Blaber', 'Nisqy', 'Zven', 'Vulcan'],
+    // ['Wunder', 'Jankos', 'Perkz', 'Caps', 'Mikyx'],
+    // ['Bwipo', 'Selfmade', 'Nemesis', 'Rekkles', 'Hylissang'],
+    // ['Alphari', 'Xerxe', 'NukeDuck', 'Upset', 'Destiny']
   ];
 
-  bench = [
-    'Sacre',
-    'Bjergsen',
-    'Biofrost'
+  benches: Player[][] = [
+    [
+      { name: 'BrokenBlade', position: 'top' },
+      { name: 'Dardoch', position: 'jng' },
+      { name: 'Bjergsen', position: 'mid' }
+    ],
+    [
+      { name: 'BrokenBlade', position: 'top' },
+      { name: 'Dardoch', position: 'jng' },
+      { name: 'Bjergsen', position: 'mid' }
+    ],
+    [
+      { name: 'BrokenBlade', position: 'top' },
+      { name: 'Dardoch', position: 'jng' },
+      { name: 'Bjergsen', position: 'mid' }
+    ]
+  ]
+
+  teams: Team[] = [
+    {
+      teamName: 'A',
+      teamLeader: 'Chris',
+      roster: this.rosters[0],
+      bench: this.benches[0]
+    },
+    {
+      teamName: 'B',
+      teamLeader: 'Vasko',
+      roster: this.rosters[1],
+      bench: this.benches[1]
+    },
+    {
+      teamName: 'c',
+      teamLeader: 'Griffin',
+      roster: this.rosters[2],
+      bench: this.benches[2]
+    }
   ];
+
+  rosterImg: string[] = [];
+  benchImg: string[] = [];
 
   drop(event: CdkDragDrop<string[]>) {
     console.log(event);
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex);
-    }
+    // if (event.previousContainer === event.container) {
+    //   moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    //   //moveItemInArray(this.roster, event.previousIndex, event.currentIndex);
+    // } else {
+    //   console.log("transferring " + event.previousContainer.data[event.previousIndex] + " to " + event.container.data + " position " + event.currentIndex);
+    //   transferArrayItem(event.previousContainer.data,
+    //     event.container.data,
+    //     event.previousIndex,
+    //     event.currentIndex);
+    // }
   }
 
   constructor() { }
 
   ngOnInit(): void {
+    // this.roster.forEach(item => {
+    //   this.rosterImg.push("assets/" + item + ".png");
+    // })
+
+    // this.bench.forEach(item => {
+    //   this.benchImg.push("assets/" + item + ".png");
+    // })
   }
 
 }
