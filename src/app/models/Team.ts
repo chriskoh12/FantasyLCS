9ab7d5026a260dfb29f7ba97642bd346edf88388
chriskoh12 <chriskoh12@gmail.com>
2020-04-18 19:18:30 -0400

    refactor classes and types

--- src/app/models/Team.ts
+++ src/app/models/Team.ts
@@ -1,4 +1,4 @@
-export class Team {
+export class FantasyTeam {
     name: string;
     coach: string;
     roster: Player[];
@@ -8,9 +8,10 @@ export class Team {
 export class Player {
     name: string;
     position: Position;
+    team: Team;
 }
 
-export enum Position {
+export enum Position { // Enum used to be able to use addition to determine cdkDropList role type
     top = 0,
     jng,
     mid,
@@ -19,3 +20,6 @@ export enum Position {
     flex,
     team
 }
+
+export type Team = 'TL' | 'TSM' | 'C9' | 'CLG' | '100T' | 'FLY' | 'GGS' | 'IMT' | 'DIG' | 'EG' |
+                   'G2' | 'FNC' | 'OG' | 'VIT' | 'MSF' | 'S04' | 'MAD' | 'SK' | 'XL' | 'RGE';
