74c769100cd8bd67674cf46bbbcfa7e8fe478f87
chriskoh12 <chriskoh12@gmail.com>
2020-05-01 13:11:35 -0400

    added current week and free agents tab

---
+++ src/app/free-agents/free-agents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-free-agents',
+  templateUrl: './free-agents.component.html',
+  styleUrls: ['./free-agents.component.css']
+})
+export class FreeAgentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
