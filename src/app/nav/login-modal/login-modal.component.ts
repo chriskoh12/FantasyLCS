5dfb36161bc81584b7f8a6b96ec5f9a909f9f7c3
chriskoh12 <chriskoh12@gmail.com>
2022-09-27 16:31:14 -0700

    add error handling to login calls

--- src/app/nav/login-modal/login-modal.component.ts
+++ src/app/nav/login-modal/login-modal.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from 'src/app/auth.service';
 import { ISignUpResult, CognitoUser } from 'amazon-cognito-identity-js';
-// import * as AWS from 'aws-sdk';
 
 @Component({
   selector: 'app-login-modal',
@@ -11,9 +10,8 @@ import { ISignUpResult, CognitoUser } from 'amazon-cognito-identity-js';
 })
 export class LoginModalComponent implements OnInit {
 
-  // @Output() userSignUp = new EventEmitter<PlayerMoveEvent>();
-  username: string;
-  password: string;
+  username: string = "";
+  password: string = "";
   email: string;
   login: boolean = true;
 
@@ -26,17 +24,28 @@ export class LoginModalComponent implements OnInit {
 
   authSignIn(): void {
     this.authService.signIn(this.username, this.password)
-      .then( signInResult => this.user = signInResult);
-      setTimeout(() => console.log(this.user), 2000);
+      .then( signInResult => {
+        this.user = signInResult;
+        console.log(this.user);
+        this.openNotification("Signed In successfully!");
+      })
+      .catch( err => {
+        this.openNotification("Incorrect username or password");
+      });
   }
 
   authSignUp(): void {
-    this.authService.signUp(this.username, this.password, this.email);
+    this.authService.signUp(this.username, this.password, this.email)
+      .then( () => this.openNotification("Signed Up successfully!"))
+      .catch( err => {
+        console.log(err)
+        this.openNotification(`Sign Up unsuccessful. Reason: ${err}`)
+      });
   }
 
-  openNotification(message: string, actionLabel: string): void {
+  openNotification(message: string, duration: number = 3000, actionLabel: string = ""): void {
     this.notification.open(message, actionLabel, {
-      duration: 5000,
+      duration: duration,
       horizontalPosition: 'center',
       verticalPosition: 'top'
       });
